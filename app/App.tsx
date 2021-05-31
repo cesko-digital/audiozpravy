7c869e41996582a29b321e76951ff2db0ee2b017
Egon Jajak <pisna.d@gmail.com>
2021-05-31 23:36:04 +0200

    DT-1 Implement heading component.  Use expo-font module. Use heading in onboarding.

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import AppLoading from "expo-app-loading";
+import { useFonts, Monda_700Bold } from "@expo-google-fonts/monda";
+
 import TopicsScreen from "./screen-components/topics";
 import { Screens } from "./screens";
 import OnboardingScreen from "./screen-components/onboarding";
@@ -8,12 +11,21 @@ import OnboardingScreen from "./screen-components/onboarding";
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={Screens.onboarding} component={OnboardingScreen} />
-        <Stack.Screen name={Screens.topics} component={TopicsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  let [fontsLoaded] = useFonts({ MondaBold: Monda_700Bold });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name={Screens.onboarding}
+            component={OnboardingScreen}
+          />
+          <Stack.Screen name={Screens.topics} component={TopicsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
