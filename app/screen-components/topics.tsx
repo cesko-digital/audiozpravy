734c6c8d55ff2f2458a9e3c1922e45baa095e9e4
Marcela <62770219+CoffeeladyCZ@users.noreply.github.com>
2021-06-08 23:28:09 +0200

    first code

--- app/screen-components/topics.tsx
+++ app/screen-components/topics.tsx
@@ -1,11 +1,77 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Image, Text, Button } from "react-native";
+import Heading from "../components/typography/heading";
+
+
+const fullCategories = [ 'Ekonomika', 'Zprávy z domova', 'Zprávy ze světa', 'Sport', 'Kultura', 'Technologie', 'Regiony', 'Zdraví', 'Koronavirus',
+  'Volby do parlamentu', 'Bitcoin', 'Myanmar', 'Vakcíny', 'Bělorusko', 'Regiony'];
+
+let buttonListArr = [];
 
 const TopicsScreen = ({ navigation }) => {
+
   return (
     <>
-      <Text>Topics</Text>
+      <View
+        style={{
+          paddingTop: 32,
+          paddingLeft: 24
+        }}
+      >
+        <Image src="../assets/arrow-long.svg"
+            style={{
+              
+              left: '83.33%',
+              right: '-50%',
+              top: '66.67%',
+              bottom: '0%'
+            }}></Image>
+        <Heading
+          style={{
+            fontSize: 32
+          }}
+        >Co vás zajímá?</Heading>
+        <Text
+          style={{
+            fontSize: 18
+          }}
+        >
+          Vyberte alespoň tři témata
+        </Text>
+        <Text>
+          Aplikace se automaticky učí, co se Vám líbí. 
+          Naučené volby můžete upravit v nastavení.
+        </Text>
+        <View>
+          {buttonListArr = fullCategories.map(category => {
+            <Button 
+              title={category}
+              style={{
+                backgroundColor: '#1745C0',
+                color: '#fff',
+                fontSize: 14,
+                lineHeight: 20,
+                paddingTop: 8,
+                paddingLeft: 16,
+                borderRadius: 40
+              }}
+              key={category.id}
+            >
+            {category}
+            </Button>
+          })}
+          
+        </View>
+        <Button 
+          title="Poslechnout si výběr"
+          onPress={() => navigation.navigate(Screens.categories)}
+        />
+      </View>
+      
+
     </>
+
+
   );
 };
 
