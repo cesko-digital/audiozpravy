112f83af007c0bb17a97caf1899a9ac83f4d38b3
Egon Jajak <pisna.d@gmail.com>
2021-06-10 23:08:13 +0200

    DT-5 Refactor onboarding screen
    DT-5 Create Login-link component. Move Onboarding screen under directory.

--- app/App.tsx
+++ app/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AppLoading from "expo-app-loading";
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 
 import TopicsScreen from "./screen-components/topics";
 import { Screens } from "./screens";
@@ -12,9 +12,9 @@ const Stack = createStackNavigator();
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    MondaBold: require('./assets/fonts/Monda-Bold.ttf'),
-    RobotoLight: require('./assets/fonts/Roboto-Light.ttf'),
-    RobotoBold: require('./assets/fonts/Roboto-Bold.ttf')
+    MondaBold: require("./assets/fonts/Monda-Bold.ttf"),
+    RobotoLight: require("./assets/fonts/Roboto-Light.ttf"),
+    RobotoBold: require("./assets/fonts/Roboto-Bold.ttf"),
   });
 
   if (!fontsLoaded) {
---
+++ app/screen-components/onboarding/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View } from "react-native";
+
+import { Screens } from "../../screens";
+import MainButton from "../../components/buttons/main-button";
+import Description from "../../components/typography/description";
+import Title from "./title";
+import LoginLink from "./login-link";
+
+const OnboardingScreen = ({ navigation }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "space-between",
+        marginTop: 120,
+        marginBottom: 56,
+      }}
+    >
+      <View style={{ marginHorizontal: 56 }}>
+        <Title />
+        <Description style={{ marginTop: 28 }}>
+          Všechny zprávy ze všech zpravodajských portálů pro Vás jako audio.
+          Pusťte si 30s ukázky zadarmo nebo si kupte kredity a poslechněte si
+          dlouhou verzi. Stačí si jen naklikat frontu zpráv nebo nechte výběr na
+          naší umělé inteligenci, která se neustále učí z Vašich reakcí.
+        </Description>
+      </View>
+
+      <View style={{ marginHorizontal: 48 }}>
+        <MainButton onPress={() => navigation.navigate(Screens.topics)}>
+          Nastavit témata
+        </MainButton>
+        <LoginLink style={{ marginTop: 32, display: "none" }} />
+      </View>
+    </View>
+  );
+};
+
+export default OnboardingScreen;
