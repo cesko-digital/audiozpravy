aad82c03b9d0d5d9857733e29f982c1b22b8e196
Egon Jajak <pisna.d@gmail.com>
2021-06-16 23:49:43 +0200

    DT-7 Use @react-navigation/bottom-tabs with custom Bar implementation.

--- app/screen-components/topics.tsx
+++ app/screen-components/topics.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Text } from "react-native";
+import MainButton from "../components/buttons/main-button";
+import { Screens } from "../screens";
 
 const TopicsScreen = ({ navigation }) => {
   return (
     <>
       <Text>Topics</Text>
+      <MainButton onPress={() => navigation.navigate(Screens.home)}>
+        Poslechnout si výběr
+      </MainButton>
     </>
   );
 };
