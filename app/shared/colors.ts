68e4a9c0d14e8ef3a90a80cd2041940119b89d48
Petra Zemanová <zemape42@gmail.com>
2021-06-21 18:49:23 +0200

    News component

--- app/shared/colors.ts
+++ app/shared/colors.ts
@@ -1,7 +1,11 @@
 enum Color {
   "black-100" = "#0D0B12",
+  "black-64" = "#565459",
+  "black-32" = "#868588",
+  "black-24" = "#ADACAE",
   "black-16" = "#CAC9CB",
   "blue" = "#1745C0",
+  "grey" = "#DBDADB",
 }
 
 export default Color;
