cec8dfa73b6333b038515d0e756d5b6d6a5aee8f
Michal Kucirka <michal.kucirka@creativedock.com>
2021-11-21 18:20:36 +0100

    Adding personal recommendation into playlist

--- backend/audionews/types/playlist.py
+++ backend/audionews/types/playlist.py
@@ -8,6 +8,7 @@ from graphene_django.filter import DjangoFilterConnectionField
 from graphql.execution.base import ResolveInfo
 from promise.promise import Promise
 
+from recommender.personal_recommend import PersonalRecommender
 from recommender.recommend import recommend_by_google_trends
 from ..models import Article, Listener, Play, Provider, Playlist, Category
 from .play import PlayNode
@@ -19,6 +20,7 @@ class PlaylistNode(DjangoObjectType):
     articles = List(NonNull(ArticleNode), required=True)
     category = NonNull(CategoryNode)
     articles_trending = List(NonNull(ArticleNode))
+    articles_recommended = List(NonNull(ArticleNode))
 
     class Meta:
         model = Playlist
@@ -63,7 +65,33 @@ class PlaylistNode(DjangoObjectType):
             return playlist.articles.all()
 
 
-    def resolve_articles_recommended(self, info, **kwargs):
+    def resolve_articles_recommended(root, info, n_of_past_days: int = 1000, n_of_recommendations: int = 30 **kwargs):
         """ Recommend articles based on user history"""
-        #TODO
-        pass
+
+        category, _ = Category.objects.get_or_create(name="recommended")
+        playlist, playlist_created = Playlist.objects.get_or_create(
+            prepared_for_date=datetime.date(datetime.now()),
+            category=category
+        )
+        if not playlist_created:
+            recommender = PersonalRecommender(user_id=root.id, embed_vector_path='s3_input/articles_embeddings.json')
+            recommendations_df = recommender.recommend(n_of_past_days=n_of_past_days)
+            for i, (_, row) in recommendations_df.iterrows():
+                provider, created = Provider.objects.get_or_create(name="Unknown", website_url="unknown")
+                category_article, created = Category.objects.get_or_create(name='unknown')
+                article, created = Article.objects.get_or_create(
+                    category=category_article,
+                    title=row.title,
+                    perex=row.summary,
+                    recording_created_at=datetime.datetime.now(),
+                    pub_date=datetime.datetime.fromtimestamp(row.pub_date),
+                    url=str(row.url),
+                    provider=provider
+                )
+                playlist.articles.add(article)
+                if i >= n_of_recommendations:
+                    break
+
+        return playlist
+
+
