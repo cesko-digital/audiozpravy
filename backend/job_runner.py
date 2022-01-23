73e89548d5a29bb438ed05650dc72f842a2a151e
Michal Kucirka <michal.kucirka@creativedock.com>
2022-01-23 12:07:52 +0100

    Adding bert for embeddings calculation

--- backend/job_runner.py
+++ backend/job_runner.py
@@ -9,7 +9,7 @@ import os
 
 from pipeline.rss_scraper.rss_scraper.sources import parse_ctidoma_category
 from pipeline.text_processing import fit_tf_idf
-from recommender.save_embeddings import calculate_doc2vec_vectors
+from recommender.save_embeddings import calculate_doc2vec_vectors, calculate_bert_vectors
 from recommender.train_doc2vec import train_w2v_model
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.settings")
@@ -23,7 +23,17 @@ from classes.trend_watcher import TrendWatcher
 from pipeline.rss_scraper.rss_scraper import SOURCES
 from pipeline.rss_scraper.rss_scraper.scraper import _scrape_feed
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+CTIDOMA_SOURCE = {
+        "name": "ctidoma.cz",
+        "feeds": [
+            {
+                "url": "https://www.ctidoma.cz/export/cesko-digital",
+                "category": parse_ctidoma_category,
+            }
+        ],
+    }
 
 class JobRunner:
     def __init__(self):
@@ -56,7 +66,9 @@ class JobRunner:
                 text=f'{entry["title"]}: {entry["summary"]}'
             )
 
+        self.logger.info(f'Adding {len(entries)} articles into db fro ctidoma')
         self.new_entries.extend(entries)
+
 
 
     def create_playlists(self, create_for_date: datetime.date, date_from: datetime.date):
@@ -77,19 +89,20 @@ class JobRunner:
         train_w2v_model(model_path)
 
 
-    def save_embeddings(self, doc2vec_path, vectors_path):
+    def save_embeddings(self, bert_folder_path, vectors_path):
         """
         calculates and seves embeddings from all vectors based on trained model on 'doc2vec_path' and saves results
         int 'vectors_path'
         """
         self.logger.info(f'Creating embeddings and saving into {vectors_path}')
-        calculate_doc2vec_vectors(doc2vec_path, vectors_path)
+        calculate_bert_vectors(bert_folder_path, vectors_path)
 
 
 if __name__ == '__main__':
     "Regular jobs that run on a daily basis. Script runs all the jobs 30 minutes after midnight each day "
     args = argparse.ArgumentParser()
     args.add_argument('--doc2vec-path', default='s3_input/doc2vec_articles.model')
+    args.add_argument('--bert-folder-path', default='audiozpravy/bert/bg_cs_pl_ru_cased_L-12_H-768_A-12_pt')
     args.add_argument('--vectors-path', default='s3_input/articles_embeddings.json')
     args.add_argument('--n-past-days', default=100000,
                       help = 'Number of days to consider for articles to recommend for categories. for value 4 we '
@@ -108,10 +121,11 @@ if __name__ == '__main__':
             runner.get_new_articles()
             date_in_past = (datetime.datetime.now() - datetime.timedelta(days=cfg.n_past_days)).date()
             runner.create_playlists(date_today, date_in_past)
-            runner.train_w2v_model(cfg.doc2vec_path)
+            # runner.train_w2v_model(cfg.doc2vec_path)
             runner.save_embeddings(cfg.doc2vec_path, cfg.vectors_path)
 
         # sleep for 30 minutes
+        logging.getLogger('Main').info('Sleep for 30 minutes ..')
         time.sleep(30 * 60)
 
 
--- backend/recommender/save_embeddings.py
+++ backend/recommender/save_embeddings.py
@@ -4,6 +4,8 @@ import logging
 import os.path
 import sqlite3
 
+from deeppavlov.core.common.file import read_json
+from deeppavlov import build_model, configs
 import django
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.settings")
@@ -40,13 +42,52 @@ def calculate_doc2vec_vectors(model_path: str, vectors_path: str) -> None:
     json.dump(article_embeddings, open(vectors_path, 'w'))
 
 
+def calculate_bert_vectors(model_path_folder: str, vectors_path: str):
+    from deeppavlov.core.common.file import read_json
+    from deeppavlov import build_model, configs
+
+    bert_config = read_json(configs.embedder.bert_embedder)
+    bert_config['metadata']['variables']['BERT_PATH'] = '/home/michal/projects/audiozpravy/bert/bg_cs_pl_ru_cased_L-12_H-768_A-12_pt'
+
+    slavic_bert = build_model(bert_config)
+
+    all_articles = Article.objects.all()
+    if os.path.exists(vectors_path):
+        article_embeddings = json.load(open(vectors_path, 'r'))
+    else:
+        article_embeddings = {}
+
+    article_ids = [article.id for article in all_articles]
+    new_articles = set(article_ids) - set(list(map(int, article_embeddings.keys())))
+
+    tokens, token_embs, subtokens, subtoken_embs, sent_max_embs, sent_mean_embs, bert_pooler_outputs = slavic_bert(
+        [article.perex for article in all_articles if article.id in new_articles]
+    )
+
+    for new_article_id, emb_vecotr in zip(new_articles, sent_mean_embs):
+        article_embeddings[str(new_article_id)] = {'tokens': tokens, 'sent_embedding': list(emb_vecotr)}
+
+    json.dump(article_embeddings, open(vectors_path, 'w'))
+
+
 if __name__ == '__main__':
     """ Script for calculating embeddings for all articles and saving them into specific file"""
     args = argparse.ArgumentParser()
-    args.add_argument('--doc2vec-path', default='s3_input/doc2vec_articles.model')
+    args.add_argument('--model-path', default='/home/michal/projects/audiozpravy/bert/bg_cs_pl_ru_cased_L-12_H-768_A-12_pt')
     args.add_argument('--vectors-path', default='s3_input/articles_embeddings.json')
+    args.add_argument('--doc2vec', default=False, action='store_true')
+    args.add_argument('--bert', default=False, action='store_true')
     cfg = args.parse_args()
-    calculate_doc2vec_vectors(cfg.doc2vec_path, cfg.vectors_path)
+    if not cfg.bert and not cfg.model:
+        AssertionError('Either --bert or --doc2vec needs to be specified')
+    if cfg.bert and cfg.doc2vec:
+        AssertionError('Only one of the models need to be specified. Set either --bert or --doc2vec')
+
+    if cfg.doc2vec:
+        calculate_doc2vec_vectors(cfg.doc2vec_path, cfg.vectors_path)
+    else:
+        calculate_bert_vectors(cfg.model_path, cfg.vectors_path)
+
 
 
 
