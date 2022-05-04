0f27ea155ff85bb3200a09c915901c2bbeec1c4d
Jakub Bares <bares.jakub@gmail.com>
2022-05-04 14:59:51 +0200

    docker

--- backend/API/settings.py
+++ backend/API/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET', 'developmentsecret')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', '1') == '1'
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 """
