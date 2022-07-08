5a39364bc12bc3829a80c8f85c67d80c80524a24
Jakub Bares <bares.jakub@gmail.com>
2022-07-08 21:33:50 +0200

    scheduler

--- backend/manage.py
+++ backend/manage.py
@@ -6,6 +6,7 @@ from job_runner.scheduler import Scheduler
 
 def main():
     """Run administrative tasks."""
+    print("Running main")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API.settings')
     try:
         from django.core.management import execute_from_command_line
